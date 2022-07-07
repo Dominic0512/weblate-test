f5be719ed92a26fd7823adde5c2b1569c0b0345e
Dominic <dominiclee0512@gmail.com>
2022-07-07 13:01:48 +0800

    feat: add locale and gettext dependencies for deployment.

--- src/index.php
+++ src/index.php
@@ -1,18 +1,32 @@
 <?
   $lang_code = $_GET['lang'];
 
-  function _i($key, $code) {
-    switch ($code) {
-      case 'en':
-      case 'ko_KR':
-      case 'zh_TW':
-        return $key . $code;
-    }
-  } 
+  $locale = 'en_US.UTF-8';
+
+  switch($lang_code) {
+    case 'kr':
+      $locale = 'ko_KR.UTF-8';
+      break;
+    case 'tw':
+      $locale = 'zh_TW.UTF-8';
+      break;
+    case 'en':
+    default: 
+      break;
+  }
+
+  $local_path =  '/app/locale';
+  echo $local_path . '</br>';
+
+  setlocale(LC_ALL, $locale);
+  bindtextdomain('main', $local_path);
+  textdomain('main');
+
+  // echo phpInfo();
   
   echo 'Current Lang Code is: ' . $lang_code . '</br>';
-  echo _i('hello world', $lang_code) . '</br>';
-  echo _i('how are you', $lang_code) . '</br>';
-  echo _i('test 8', $lang_code) . '</br>';
-  echo _i('demo 3', $lang_code) . '</br>';
+  echo _('hello world') . '</br>';
+  echo _('how are you') . '</br>';
+  echo _('test 8') . '</br>';
+  echo _('demo 3') . '</br>';
 ?>
