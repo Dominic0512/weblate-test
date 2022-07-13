497f851b80a9af2d69a17e1b91aea408fe7c9e9a
Dominic <dominiclee0512@gmail.com>
2022-07-13 11:06:25 +0800

    feat: parallel workflows test 4.

--- src/admin/index.php
+++ src/admin/index.php
@@ -30,7 +30,7 @@
     <div>
       <h3>Admin</h3>
       <?=_('This is admin section')?>
-      <?=_('Admin parallel test 3')?>
+      <?=_('Admin parallel test 4')?>
     </div>
   </body>
 </html>
