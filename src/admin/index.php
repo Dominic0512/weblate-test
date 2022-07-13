484775503c62e5bc0778c0226e4d16129f311753
Dominic <dominiclee0512@gmail.com>
2022-07-13 09:51:31 +0800

    feat: separate page for different locale scope.

--- src/admin/index.php
+++ src/admin/index.php
@@ -15,14 +15,44 @@
       break;
   }
 
-  $local_path =  '/app/locale/admin';
+  $locale_path =  '/app/locale/admin';
   setlocale(LC_ALL, $locale);
-  bindtextdomain('admin', $local_path);
-  textdomain('admin');
+  bindtextdomain('main', $locale_path);
+  textdomain('main');
 ?>
 
-<div>
-  <h3>Admin</h3>
-  <?=_('This is admin section')?>
-  <?=_('Admin test 1')?>
-</div>
+<html>
+  <head></head>
+  <body class="main-container">
+    <h1><?=_('This is weblate demo test')?></h1>
+    <h2><?=_('Current language code is') . ' [' . $lang_code . ']'?></h2>
+    <hr/>
+    <div>
+      <h3>Admin</h3>
+      <?=_('This is admin section')?>
+      <?=_('Admin test 3')?>
+    </div>
+  </body>
+</html>
+
+<style>
+  hr {
+    border: 0;
+    clear:both;
+    display:block;
+    width: 96%;               
+    background-color: black;
+    height: 1px;
+  }
+  .main-container {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+    text-align: center;
+  }
+
+</style>
--- src/index.php
+++ src/index.php
@@ -1,75 +1,21 @@
 <?
   $lang_code = $_GET['lang'];
-
-  $locale = 'en_US.UTF-8';
-
-  switch($lang_code) {
-    case 'kr':
-      $locale = 'ko_KR.UTF-8';
+  $curdir = dirname($_SERVER['REQUEST_URI']) . '/';
+  $baseUri = str_replace($curdir, '/', $curdir);
+  switch ($baseUri) {
+    case '/':
+    case '/main' :
+      require __DIR__ . '/main/index.php';
       break;
-    case 'tw':
-      $locale = 'zh_TW.UTF-8';
+    case '/site' :
+      require __DIR__ . '/site/index.php';
       break;
-    case 'en':
-    default: 
+    case '/admin' :
+      require __DIR__ . '/admin/index.php';
+      break;
+    default:
+      http_response_code(404);
+      require __DIR__ . '/404.php';
       break;
   }
-
-  $local_path =  '/app/locale';
-  setlocale(LC_ALL, $locale);
-  bindtextdomain('main', $local_path);
-  textdomain('main');
 ?>
-
-<html>
-  <head></head>
-  <body class="main-container">
-    <h1><?=_('This is weblate demo test')?></h1>
-    <h2><?=_('Current language code is') . ' [' . $lang_code . ']'?></h2>
-    <hr/>
-
-    <div class="section-container">
-      <div class="section">
-        <? include('./main/index.php') ?>
-      </div>
-      <div class="section">
-        <? include('./admin/index.php') ?>
-      </div>
-      <div class="section">
-        <? include('./site/index.php') ?>
-      </div>
-    </div>
-   
-  </body>
-</html>
-
-<style>
-  hr {
-    border: 0;
-    clear:both;
-    display:block;
-    width: 96%;               
-    background-color: black;
-    height: 1px;
-  }
-  .main-container {
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin: auto;
-    text-align: center;
-  }
-
-  .section-container {
-    width: 100vw;
-    display: flex;
-  }
-
-  .section {
-    flex-grow: 1;
-  }
-
-</style>
