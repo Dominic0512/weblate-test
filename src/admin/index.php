32bbd6f3a386f5eaa07be6583c9a2891c8e0c7b3
Dominic <dominiclee0512@gmail.com>
2022-07-13 13:08:49 +0800

    feat: parallel workflows test 6.

--- src/admin/index.php
+++ src/admin/index.php
@@ -30,7 +30,7 @@
     <div>
       <h3>Admin</h3>
       <?=_('This is admin section')?>
-      <?=_('Admin parallel test 5')?>
+      <?=_('Admin parallel test 6')?>
     </div>
   </body>
 </html>
--- src/main/index.php
+++ src/main/index.php
@@ -30,7 +30,7 @@
     <div>
       <h3>Main</h3>
       <?=_('This is main section')?>
-      <?=_('Main parallel test 5')?>
+      <?=_('Main parallel test 6')?>
     </div>
   </body>
 </html>
